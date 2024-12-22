88b8d35a9da3deaacc60b5dad37dc4e705c3c07d
Ginikachukwu Nwokeji <166800413+Chrimau@users.noreply.github.com>
2024-12-22 04:56:47 +0100

    added resposiveness for slides in  script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
 
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
 function changeSlide(direction) {
     currentSlide += direction;
     if (currentSlide < 0) currentSlide = slides.length - 1;
